6fbb0b3c918951567982b84b8596943bd00dcc17
Thomas Trost <tomtrost@sbcglobal.net>
2020-12-01 10:20:53 -0500

    Added Location and Department descriptions to seed method.

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -306,9 +306,347 @@ namespace COVID19TriC.Migrations
 
             );
 
+            context.Locations.AddOrUpdate(l => l.LocationDescription,
+            new Location
+            {
+                LocationDescription = "Advanced Technology Training Center"
+            },
 
+            new Location
+            {
+                LocationDescription = "Brunswick University Center"
+            },
 
+            new Location
+            {
+                LocationDescription = "Corporate College East"
+            },
 
+            new Location
+            {
+                LocationDescription = "Corporate College West"
+            },
+
+            new Location
+            {
+                LocationDescription = "District Administrative Offices"
+            },
+
+            new Location
+            {
+                LocationDescription = "Eastern Campus"
+            },
+
+            new Location
+            {
+                LocationDescription = "Hospitality Management Center"
+            },
+
+            new Location
+            {
+                LocationDescription = "Jerry Sue Thornton Center"
+            },
+
+            new Location
+            {
+                LocationDescription = "Manufacturing Technology Center"
+            },
+
+            new Location
+            {
+                LocationDescription = "Metropolitan Campus"
+            },
+
+            new Location
+            {
+                LocationDescription = "Public Safety Training Center"
+            },
+
+            new Location
+            {
+                LocationDescription = "Transportation Innovation Center"
+            },
+
+            new Location
+            {
+                LocationDescription = "Western Campus"
+            },
+
+            new Location
+            {
+                LocationDescription = "Westshore Campus"
+            }
+
+            );
+
+            context.Departments.AddOrUpdate(d => d.DepartmentDescription,
+            new Department
+            {
+                DepartmentDescription = "Accounting"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Adult Diploma Program"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "American Sign Language"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Applied Industrial Technology"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Aspire"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Art"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Science"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Automotive Technology"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Business Management"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Captioning And Court Reporting"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Cisco"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Conflict Resolution And Peace Studies"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Corporate College"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Creative Arts"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Criminal Justice"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Deaf Interpretive Services"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Early Childhood Education"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Engineering Technology"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "English As A Second Language"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Fire"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Health Careers"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Honors & Scholars"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Hospitality Management"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Information Technology"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Liberal Arts, Humanities, Social, and Behavioral Sciences"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Manufacturing"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Nursing"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Online Training"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Paralegal"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Plant Science And Landscape Technology"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Public Safety"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Senior Adult Education"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "STEM"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Study Abroad"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Teacher Education"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Transportation"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Transportation Center"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Welding"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Workforce"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Office of the President"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Board of Trustees Office"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Business Continuity"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Capital and Construction"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Customer Service"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Foundation"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Government Relations"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Human Resources"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Information Technology Services"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Office of Legal Services"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Plant Operations"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Integrated Communications"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Supplier Managed Services"
+            },
+
+            new Department
+            {
+                DepartmentDescription = "Campus Police & Security Services"
+            }
+            );
+
+                      
         }
     }
 }
