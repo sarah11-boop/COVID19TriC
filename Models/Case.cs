d222ec2826f6245f18b96635d9f89c2ec2aeb560
Eduardo Mandujano <65335115+Flutegod007@users.noreply.github.com>
2020-12-02 18:23:03 -0500

    Update Case.cs

--- Models/Case.cs
+++ Models/Case.cs
@@ -10,39 +10,54 @@ namespace COVID19TriC.Models
     public class Case
     {
         public int CaseID { get; set; }
+        [Required]
         public bool Quarantined { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateQuarantineEnds { get; set; }
+        [Required]
         public bool CountyNotified { get; set; }
+        [Required]
         public string GeneralComments { get; set; }
+        [Required]
         public string ExposureComments { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateLastAtLocation { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfExposure { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfSymptoms { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfTest { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime DateOfDiagnosis { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateReported { get; set; }
+        [Required]
         public string Symptoms { get; set; }
+        [Required]
         public string ReportingName { get; set; }
+        [Required]
         public string ReportingTitle { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateCreated { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateModified { get; set; }
         public int CreatedBy { get; set; }
         public int ModifiedBy { get; set; }
