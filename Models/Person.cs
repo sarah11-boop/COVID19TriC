58a267ab33091da39476d71ce5ce1a102fd76eef
Eduardo Mandujano <65335115+Flutegod007@users.noreply.github.com>
2020-12-02 18:26:16 -0500

    Update Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -12,19 +12,27 @@ namespace COVID19TriC.Models
     public class Person
     {
         public int PersonID { get; set; }
+        [Required]
         public string SNumber { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string MiddleName { get; set; }
         public enum PersonnelType { Student, Faculty, Staff }
+        [Required]
         public string PersonalEmail { get; set; }
         public string SchoolEmail { get; set; }
         public string PhoneNumber { get; set; }
 
-        [DataType(DataType.Date)]
+        
+         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateCreated { get; set; }
 
         [DataType(DataType.Date)]
+         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateModified { get; set; }
         public int CreatedBy { get; set; }
         public int ModifiedBy { get; set; }
